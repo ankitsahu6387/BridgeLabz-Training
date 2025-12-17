4c12dbd8c8f9a46f99b89f67cc4998db1a0767a5
Ankit Sahu <ankits6387@gmail.com>
2025-12-17 16:48:03 +0530

    [Ankit Sahu] area of rectangle

---
+++ core-java-practice/gcr-codebase/programming-element/perimeterOfRectangle.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class perimeterOfRectangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the length: ");    
+        int length = sc.nextInt();
+        System.out.print("Enter the width: ");
+        int width = sc.nextInt(); 
+        int perimeter = 2 * (length + width);
+        System.out.println("The perimeter of the rectangle is: " + perimeter);
+    }
+}
