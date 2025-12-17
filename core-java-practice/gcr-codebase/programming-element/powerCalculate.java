f7d279ef2501a476286748c3608cf3d85da4c279
Ankit Sahu <ankits6387@gmail.com>
2025-12-17 16:42:53 +0530

    [Ankit Sahu] power calculate

---
+++ core-java-practice/gcr-codebase/programming-element/powerCalculate.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class powerCalculate {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the base number: ");    
+        int base = sc.nextInt();
+        System.out.print("Enter the exponent: ");
+        int exponent = sc.nextInt();
+        int result = (int) Math.pow(base, exponent);      
+        System.out.println(base + " raised to the power of " + exponent + " is: " + result);
+    }
+}
