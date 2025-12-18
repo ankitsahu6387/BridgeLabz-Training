44838dfed63d08ede2b0cd56f33b412c7e649db4
Ankit Sahu <ankits6387@gmail.com>
2025-12-18 12:35:03 +0530

    [Ankit Sahu] added new file

---
+++ core-java-practice/leet-code-codebase/PrimeNumCheck.java
@@ -0,0 +1,15 @@
+public class PrimeNumCheck {
+    public static boolean isPrime(int num){
+        //optimized method
+        for(int i=2; i<Math.sqrt(num); i++)
+        {
+            if(num%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String args[]){
+        System.out.println(isPrime(6));
+    }
+}
