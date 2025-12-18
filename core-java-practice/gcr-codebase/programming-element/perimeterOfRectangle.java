011006a5f1e70a8c617b8f8cedff2a9e3c849860
Ankit Sahu <ankits6387@gmail.com>
2025-12-19 00:51:09 +0530

    [Ankit Sahu] modified

--- core-java-practice/gcr-codebase/programming-element/perimeterOfRectangle.java
+++ core-java-practice/gcr-codebase/programming-element/perimeterOfRectangle.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class PerimeterOfRectangle {
+public class perimeterOfRectangle {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the length: ");    
--- core-java-practice/gcr-codebase/programming-element/powerCalculate.java
+++ core-java-practice/gcr-codebase/programming-element/powerCalculate.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class PowerCalculate {
+public class powerCalculate {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the base number: ");    
