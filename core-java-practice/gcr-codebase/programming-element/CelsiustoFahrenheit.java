496758a94d7b4e55b22ce259aabc51a2cc94fa1a
Ankit Sahu <ankits6387@gmail.com>
2025-12-19 11:16:14 +0530

    [Ankit Sahu] new questions

---
+++ core-java-practice/gcr-codebase/programming-element/CelsiustoFahrenheit.java
@@ -0,0 +1,7 @@
+public class CelsiustoFahrenheit {
+    public static void main(String[] args) {
+        double celsius = 37.0;
+        double fahrenheit = (celsius * 9/5) + 32;
+        System.out.print(celsius + " degree Celsius is equal to " + fahrenheit + " degree Fahrenheit");
+    }
+}
