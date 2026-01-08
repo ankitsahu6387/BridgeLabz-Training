bf405646bcf466d1f20491bda85ded3e57f51095
Ankit Sahu <ankits6387@gmail.com>
2026-01-08 12:53:39 +0530

    [Ankit Sahu] Add .Added new question student record management

--- dsa-practice/gcr-codebase/linked-list/StudentRecordManagement.java
+++ dsa-practice/gcr-codebase/linked-list/StudentRecordManagement.java
@@ -1,5 +1,126 @@
+class StudentNode{
+    int rollNo;
+    String name;
+    int age;
+    char grade;
+    StudentNode next;
+    StudentNode(int rollNo, String name, int age, char grade){
+        this.rollNo = rollNo;
+        this.name = name;
+        this.age = age;
+        this.grade = grade;
+        this.next = null;
+    }
+}
+class StudentLinkedList{
+    private StudentNode head;
+    //add at start
+    void addAtBeginning(int rollNo, String name, int age, char grade){
+        StudentNode newNode = new StudentNode(rollNo, name, age, grade);
+        newNode.next = head;
+        head = newNode;
+    }
+    //add at end
+    void addAtEnd(int rollNo, String name, int age, char grade){
+        StudentNode newNode = new StudentNode(rollNo, name, age, grade);
+        if(head == null){
+            head = newNode;
+            return;
+        }
+        StudentNode temp = head;
+        while(temp.next != null){
+            temp = temp.next;
+        }
+        temp.next = newNode;
+    }
+    //Add at specific position (1-based)
+    void addAtPosition(int pos, int rollNo, String name, int age, char grade){
+        if(pos <= 1){
+            addAtBeginning(rollNo, name, age, grade);
+            return;
+        }
+        StudentNode newNode = new StudentNode(rollNo, name, age, grade);
+        StudentNode temp = head;
+        for(int i=1; i<pos-1 && temp!=null; i++){
+            temp = temp.next;
+        }
+        if(temp == null){
+            System.out.println("Invalid position");
+            return;
+        }
+        newNode.next = temp.next;
+        temp.next = newNode;
+    }
+    void deleteByRollNo(int rollNo){
+        if(head == null) return;
+        if(head.rollNo == rollNo){
+            head = head.next;
+            return;
+        }
+        StudentNode temp = head;
+        while (temp.next != null && temp.next.rollNo != rollNo) {
+            temp = temp.next;            
+        }
+        if(temp.next == null){
+            System.out.println("Student not found");
+        } else {
+            temp.next = temp.next.next;
+        }
+    }
+    // Search by roll number
+    void search(int rollNo){
+        StudentNode temp = head;
+        while(temp != null){
+            if(temp.rollNo == rollNo){
+                System.out.println("Found: " + temp.rollNo + ", " + temp.name +
+                        ", Age: " + temp.age + ", Grade: " + temp.grade);
+                return;
+            }
+            temp = temp.next;
+        }
+        System.out.println("Student not found");
+    }
+    //update grade
+    void updateGrade(int rollNo, char newGrade){
+        StudentNode temp = head;
+        while(temp != null){
+            if(temp.rollNo == rollNo){
+                temp.grade = newGrade;
+                System.out.println("Grade updated successfully");
+                return;
+            }
+            temp = temp.next;
+        }
+        System.out.println("Student not found");
+    }
+    void display(){
+        StudentNode temp = head;
+        if(temp == null){
+            System.out.println("No studen record available");
+            return;
+        }
+        while(temp != null){
+            System.out.println(temp.rollNo + " | " + temp.name +
+                    " | Age: " + temp.age + " | Grade: " + temp.grade);
+            temp = temp.next;
+        }
+    }
+}
+
 public class StudentRecordManagement{
     public static void main(String[] args) {
-        
+        StudentLinkedList list = new StudentLinkedList();
+        list.addAtBeginning(1, "Ankit", 21, 'A');
+        list.addAtEnd(2, "Rahul", 22, 'B');
+        list.addAtPosition(2, 3, "Aman", 20, 'A');
+
+        list.display();
+
+        list.search(2);
+        list.updateGrade(2, 'A');
+        list.deleteByRollNo(1);
+
+        System.out.println("\nAfter Updates:");
+        list.display();    
     }
 }
