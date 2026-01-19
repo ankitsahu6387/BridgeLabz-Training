87fd8f64be6061fb05b8954477c76b52bb61d6c8
Ankit Sahu <ankits6387@gmail.com>
2026-01-19 12:28:05 +0530

    [Ankit Sahu] Add . Added Address book UC 1

---
+++ oops-practice/gcr-codebase/address-book/ContactPerson.java
@@ -0,0 +1,22 @@
+public class ContactPerson {
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String city;
+    private String state;
+    private String zip;
+    private String phone;
+    private String email;
+
+    // Constructor
+    public ContactPerson(String firstName, String lastName, String address, String city, String state, String zip, String phone, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phone = phone;
+        this.email = email;
+    }
+}
